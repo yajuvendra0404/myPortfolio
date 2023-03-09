2505e13cbe245e0da0065211bd1377feecd64679
yajuvendra <yajuvendra0404@gmail.com>
2023-03-09 09:50:31 -0500

    navigation toggle

--- src/app/header/nav/nav.directive.ts
+++ src/app/header/nav/nav.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { ElementSchemaRegistry } from '@angular/compiler';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appNav]'
 })
 export class NavDirective {
-  
-  constructor() { }
 
+  constructor(private eleRef : ElementRef, private renedere2: Renderer2) { }
+  @HostListener( 'document:scroll' ) onScroll (){
+    console.log(window.scrollY)
+    if(window.scrollY > 360) {
+      this.renedere2.removeClass(this.eleRef.nativeElement, 'hide-sticky-nav')
+    } else {
+      this.renedere2.addClass(this.eleRef.nativeElement, 'hide-sticky-nav')
+    }
+  }
 }
