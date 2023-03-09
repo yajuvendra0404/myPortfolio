2a6871f59239df5470e9641ba9100c2d02344de2
yajuvendra <yajuvendra0404@gmail.com>
2023-03-09 15:00:58 -0500

    home page creation

--- src/app/home/my-brief/my-brief.component.ts
+++ src/app/home/my-brief/my-brief.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyBriefComponent implements OnInit {
 
+  personalInfo:any = [
+    { class:"bi bi-calendar-event mt-2", info:"Date of birth - 04/04/1992" },
+    { class:"bi bi-envelope-at mt-2", info:"Email - yajuvendra0404@gmail.com" },
+    { class:"bi bi-telephone mt-2", info:"Phone - +1(437)9960910" },
+    { class:"bi bi-geo-alt mt-2", info:"Date of birth - Ontario (Canada)" },
+  ];
+  expertise: any = [
+    { class:"bi bi-globe mt-2", info:"" },
+    { class:"bi bi-envelope-at mt-2", info:"" },
+    { class:"bi bi-telephone mt-2", info:"" },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
