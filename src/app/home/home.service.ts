890073a722284cdc4083131ca46904a708c86e0b
yajuvendra <yajuvendra0404@gmail.com>
2023-03-12 17:20:25 -0400

    My skill module updated

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -19,9 +19,8 @@ export class HomeService {
         new StrengthModel("Persistent ","#9dfc9d","#32cd32","white", "zoom-in","2000"), //9dfc9d
         new StrengthModel("Observant","#fcd29a","#ffae42","white", "zoom-in","2500"), //fcd29a
     ]
-    // bgColorShadow, bgColor, textColor, animationMotion, animationDuration
     this.skills = [
-      //
+
       new SkillsModel(0,'Javascript', 95, "rgba(251,211,4)","url(" + icons.javascript + ") no-repeat"),
       new SkillsModel(1,'Typescript', 92, "rgba(12,100,172)","url(" + icons.typescript + ") no-repeat"),
       new SkillsModel(2,'JQuery', 55, "rgba(24,100,172)" , "url(" + icons.jquery + ") no-repeat"),
@@ -33,7 +32,6 @@ export class HomeService {
       new SkillsModel(8,'Docker', 60, "rgba(4,84,156)" , "url(" + icons.docker + ") no-repeat" ),
       new SkillsModel(9,'MongoDB', 65, "rgba(108,164,108)" , "url(" + icons.mongoDB + ") no-repeat" ),
       new SkillsModel(10,'MySql', 65, "rgba(4,124,112)" , "url(" + icons.mysql + ") no-repeat" ),
-      // new SkillsModel(11,'JSON', 90, "rgba(255, 90, 25)" , icons.json),
     ]
   }
 
