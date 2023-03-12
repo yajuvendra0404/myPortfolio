bb7dfcd00d5884136449fdb75e6488ceb53754ec
yajuvendra <yajuvendra0404@gmail.com>
2023-03-12 17:31:39 -0400

    My skill module updated

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -27,11 +27,11 @@ export class HomeService {
       new SkillsModel(3,'Scss', 60, "rgba(248,100,148)" , "url(" + icons.scss + ") no-repeat" ),
       new SkillsModel(4,'Angular7+', 90, "rgba(220, 44, 4)" , "url(" + icons.angular + ") no-repeat" ),
       new SkillsModel(5,'NodeJs', 55, "rgba(40, 164, 100)" , "url(" + icons.nodejs + ") no-repeat" ),
-      new SkillsModel(6,'Java', 35, "rgba(192,84,128)" , "url(" + icons.java + ") no-repeat" ),
+      new SkillsModel(6,'Java', 35, "rgba(244,68,52)" , "url(" + icons.java + ") no-repeat" ),
       new SkillsModel(7,'Azure', 50, "rgba(44,156,227)" , "url(" + icons.azure + ") no-repeat" ),
       new SkillsModel(8,'Docker', 60, "rgba(4,84,156)" , "url(" + icons.docker + ") no-repeat" ),
-      new SkillsModel(9,'MongoDB', 65, "rgba(108,164,108)" , "url(" + icons.mongoDB + ") no-repeat" ),
-      new SkillsModel(10,'MySql', 65, "rgba(4,124,112)" , "url(" + icons.mysql + ") no-repeat" ),
+      new SkillsModel(9,'MongoDB', 65, "rgba(100,80,56)" , "url(" + icons.mongoDB + ") no-repeat" ),
+      new SkillsModel(10,'MySql', 65, "rgba(248,124,64)" , "url(" + icons.mysql + ") no-repeat" ),
     ]
   }
 
