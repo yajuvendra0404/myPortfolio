f6435ef2871337b42f7eae4ef5c3c05965ce996a
yajuvendra <yajuvendra0404@gmail.com>
2023-03-12 17:15:41 -0400

    My skills module updated

--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -22,17 +22,17 @@ export class HomeService {
     // bgColorShadow, bgColor, textColor, animationMotion, animationDuration
     this.skills = [
       //
-      new SkillsModel(0,'Javascript', 95, "rgba(255, 90, 25)","url(" + icons.javascript + ") no-repeat"),
-      new SkillsModel(1,'Typescript', 92, "rgba(255, 90, 25)","url(" + icons.typescript + ") no-repeat"  ),
-      new SkillsModel(2,'JQuery', 55, "rgba(255, 90, 25)" , "url(" + icons.jquery + ") no-repeat"),
-      new SkillsModel(3,'Scss', 60, "rgba(255, 90, 25)" , "url(" + icons.scss + ") no-repeat" ),
-      new SkillsModel(4,'Angular2+', 90, "rgba(255, 90, 25)" , "url(" + icons.angular + ") no-repeat" ),
-      new SkillsModel(5,'NodeJs', 55, "rgba(255, 90, 25)" , "url(" + icons.nodejs + ") no-repeat" ),
-      new SkillsModel(6,'Java', 35, "rgba(255, 90, 25)" , "url(" + icons.java + ") no-repeat" ),
-      new SkillsModel(7,'Azure', 50, "rgba(255, 90, 25)" , "url(" + icons.azure + ") no-repeat" ),
-      new SkillsModel(8,'Docker', 60, "rgba(255, 90, 25)" , "url(" + icons.docker + ") no-repeat" ),
-      new SkillsModel(9,'MongoDB', 65, "rgba(255, 90, 25)" , "url(" + icons.mongoDB + ") no-repeat" ),
-      new SkillsModel(10,'MySql', 65, "rgba(255, 90, 25)" , "url(" + icons.mysql + ") no-repeat" ),
+      new SkillsModel(0,'Javascript', 95, "rgba(251,211,4)","url(" + icons.javascript + ") no-repeat"),
+      new SkillsModel(1,'Typescript', 92, "rgba(12,100,172)","url(" + icons.typescript + ") no-repeat"),
+      new SkillsModel(2,'JQuery', 55, "rgba(24,100,172)" , "url(" + icons.jquery + ") no-repeat"),
+      new SkillsModel(3,'Scss', 60, "rgba(248,100,148)" , "url(" + icons.scss + ") no-repeat" ),
+      new SkillsModel(4,'Angular7+', 90, "rgba(220, 44, 4)" , "url(" + icons.angular + ") no-repeat" ),
+      new SkillsModel(5,'NodeJs', 55, "rgba(40, 164, 100)" , "url(" + icons.nodejs + ") no-repeat" ),
+      new SkillsModel(6,'Java', 35, "rgba(192,84,128)" , "url(" + icons.java + ") no-repeat" ),
+      new SkillsModel(7,'Azure', 50, "rgba(44,156,227)" , "url(" + icons.azure + ") no-repeat" ),
+      new SkillsModel(8,'Docker', 60, "rgba(4,84,156)" , "url(" + icons.docker + ") no-repeat" ),
+      new SkillsModel(9,'MongoDB', 65, "rgba(108,164,108)" , "url(" + icons.mongoDB + ") no-repeat" ),
+      new SkillsModel(10,'MySql', 65, "rgba(4,124,112)" , "url(" + icons.mysql + ") no-repeat" ),
       // new SkillsModel(11,'JSON', 90, "rgba(255, 90, 25)" , icons.json),
     ]
   }
