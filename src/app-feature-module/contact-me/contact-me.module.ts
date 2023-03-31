c24d9b0b98f911e9768167f0360bebf2479fb92f
yajuvendra <yajuvendra0404@gmail.com>
2023-03-30 20:34:31 -0400

    chat box css changes

--- src/app-feature-module/contact-me/contact-me.module.ts
+++ src/app-feature-module/contact-me/contact-me.module.ts
@@ -4,11 +4,13 @@ import { ContactMeRoutingModule } from '@contactMe/contact-me-routing.module';
 import { ContactMeComponent } from '@contactMe/contact-me.component';
 import { AppCommonModule } from '@common-module/app-common-module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { MySocialMediaPlatformComponent } from './my-social-media-platform/my-social-media-platform.component';
+import { MySocialMediaPlatformComponent } from '@contactMe/my-social-media-platform/my-social-media-platform.component';
+import { ContactFormComponent } from '@contactMe/contact-form/contact-form.component';
 @NgModule({
   declarations: [
     ContactMeComponent,
-    MySocialMediaPlatformComponent
+    MySocialMediaPlatformComponent,
+    ContactFormComponent
   ],
   imports: [
     CommonModule,
