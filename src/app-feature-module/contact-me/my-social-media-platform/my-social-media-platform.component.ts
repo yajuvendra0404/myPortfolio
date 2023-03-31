a878a53a711f5985f1aa3b4861f811d680222d45
yajuvendra <yajuvendra0404@gmail.com>
2023-03-30 20:08:43 -0400

    contact me page updated

--- src/app-feature-module/contact-me/my-social-media-platform/my-social-media-platform.component.ts
+++ src/app-feature-module/contact-me/my-social-media-platform/my-social-media-platform.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Icons } from '@assets/icons';
+import { Images } from '@assets/images';
 @Component({
   selector: 'app-my-social-media-platform',
   templateUrl: './my-social-media-platform.component.html',
   styleUrls: ['./my-social-media-platform.component.scss']
 })
-export class MySocialMediaPlatformComponent {
+export class MySocialMediaPlatformComponent implements OnInit{
+
+  // facebook, github, gmail, instagram, linkedin
+
+  facebook: string ="";
+  github: string ="";
+  gmail: string ="";
+  instagram: string ="";
+  linkedin: string ="";
+  socialMediaImg: string ="";
+
+  constructor(
+    private _icons: Icons,
+    private _images: Images
+    ) {  }
+
+  ngOnInit(): void {
+    this.facebook = this._icons.facebook;
+    this.github = this._icons.github;
+    this.gmail = this._icons.gmail;
+    this.instagram = this._icons.instagram;
+    this.linkedin = this._icons.linkedin;
+    this.socialMediaImg = this._images.socialMediaImg;
+  }
 
 }
--- src/app/assets/images.ts
+++ src/app/assets/images.ts
@@ -11,12 +11,11 @@ export class Images {
   profileImage: string = `${this.PATH}image.jpg`;
   myselgImage: string = `${this.PATH}myself.jpg`;
 
-
-
   ////// ----- Header Images ----- ////////
   headerImgHome: string = `${this.PATH}pexels-fox-desktop.jpg`; // finalized
   headerImgAboutMe: string = `${this.PATH}pexels-about.jpg`; // finalized
-  headerImgContact: string = `${this.PATH}pexels-sharad.jpg`;
-  headerImgPortfolio: string = `${this.PATH}pexels-burst.jpg`;
+  headerImgContact: string = `${this.PATH}pexels-sharad.jpg`; // finalized
+  headerImgPortfolio: string = `${this.PATH}pexels-burst.jpg`; // finalized
+  socialMediaImg: string = `${this.PATH}pexels-social-media.jpg`; // finalized
 
 }
