0748f0c652e9efb141d6ee7e728d5bb13e9a8b38
yajuvendra <yajuvendra0404@gmail.com>
2023-04-06 12:31:11 -0400

    link corrections

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-
+  {path:"",redirectTo:"/home",pathMatch:"full"},
   {path:"home", loadChildren:() => import("@home/home.module").then(m => m.HomeModule)},
   {path:"aboutMe", loadChildren:() => import("@aboutMe/about-me.module").then( m => m.AboutMeModule)},
   {path:"contactMe", loadChildren:() => import("@contactMe/contact-me.module").then( m => m.ContactMeModule)},
   {path:"portfolio", loadChildren:() => import("@project/project.module").then(m => m.ProjectModule)},
   {path:"notFound",  loadChildren:() => import("@common-module/app-common-module").then(m => m.AppCommonModule)},
-  {path:"",redirectTo:"/home",pathMatch:"full"},
   {path:"**",redirectTo:"/notFound"},
 
 ];
