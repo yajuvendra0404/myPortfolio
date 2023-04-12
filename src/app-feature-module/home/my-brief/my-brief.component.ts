115e2f0bc80c698e56812bb12de0b7381bad22c4
yajuvendra <yajuvendra0404@gmail.com>
2023-04-12 09:23:29 -0400

    my expertise added

--- src/app-feature-module/home/my-brief/my-brief.component.ts
+++ src/app-feature-module/home/my-brief/my-brief.component.ts
@@ -8,15 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class MyBriefComponent implements OnInit {
 
   personalInfo:any = [
-    { class:"bi bi-calendar-event mt-2", info:"Date of birth - 04/04/1992" },
-    { class:"bi bi-envelope-at mt-2", info:"Email - yajuvendra0404@gmail.com" },
-    { class:"bi bi-telephone mt-2", info:"Phone - +1(437)9960910" },
-    { class:"bi bi-geo-alt mt-2", info:"Location - Ontario (Canada)" },
+    { class:"bi bi-calendar-event m-2 ", info:"Date of birth - 04/04/1992" },
+    { class:"bi bi-envelope-at m-2 ", info:"Email - yajuvendra0404@gmail.com" },
+    { class:"bi bi-telephone m-2 ", info:"Phone - +1(437)9960910" },
+    { class:"bi bi-geo-alt m-2 ", info:"Location - Ontario (Canada)" },
   ];
   expertise: any = [
-    { class:"bi bi-globe mt-2", info:"" },
-    { class:"bi bi-envelope-at mt-2", info:"" },
-    { class:"bi bi-telephone mt-2", info:"" },
+    { class:"bi bi-droplet-fill m-2 ", info:"User Experience Development (Ux)" },
+    { class:"bi bi-globe m-2 ", info:"Web Developement" },
+    { class:"bi bi-braces-asterisk m-2 ", info:"Backend Development" },
   ]
 
   constructor() { }
